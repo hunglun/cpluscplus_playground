5d6ef24d195d65e95febff30a22ff61569730cd6
user <local@minipc>
2022-04-24 15:08:55 +0800

    e17 : refactor

--- euler_problem_17.cpp
+++ euler_problem_17.cpp
@@ -2,11 +2,13 @@
 #include <doctest.h>
 #include <iostream>
 #include <map>
+#include <cassert>
 using namespace std;
 
 
 #define MAP_INT_STRING map <int, string>
-string number_to_words(int n){
+
+string single_digit_to_letters(int n){
   MAP_INT_STRING digit_to_words;
   digit_to_words.insert(make_pair(1,"one"));
   digit_to_words.insert(make_pair(2,"two"));
@@ -17,32 +19,35 @@ string number_to_words(int n){
   digit_to_words.insert(make_pair(7,"seven"));
   digit_to_words.insert(make_pair(8,"eight"));
   digit_to_words.insert(make_pair(9,"nine"));
-  
   MAP_INT_STRING::const_iterator found_element = digit_to_words.find(n);
   if (found_element != digit_to_words.end()){
     return found_element->second;
   }
+  
+  assert(false); //should not be here
+  return string("");
+}
+
+string number_to_words(int n){
+  if (n > 0 && n < 10)   return single_digit_to_letters(n);
   return string("one");
 }
+
 
 int number_of_letters(int n){
   return number_to_words(n).size();
 }
 
-int single_digit_to_letters(int n){
-  return number_of_letters(n);
-}
-
 TEST_CASE("test_single_digit_to_letters"){
-  CHECK(single_digit_to_letters(1) == 3);
-  CHECK(single_digit_to_letters(2) == 3);
-  CHECK(single_digit_to_letters(3) == 5);
-  CHECK(single_digit_to_letters(4) == 4);
-  CHECK(single_digit_to_letters(5) == 4);
-  CHECK(single_digit_to_letters(6) == 3);
-  CHECK(single_digit_to_letters(7) == 5);
-  CHECK(single_digit_to_letters(8) == 5);
-  CHECK(single_digit_to_letters(9) == 4);  
+  CHECK(single_digit_to_letters(1).size() == 3);
+  CHECK(single_digit_to_letters(2).size() == 3);
+  CHECK(single_digit_to_letters(3).size() == 5);
+  CHECK(single_digit_to_letters(4).size() == 4);
+  CHECK(single_digit_to_letters(5).size() == 4);
+  CHECK(single_digit_to_letters(6).size() == 3);
+  CHECK(single_digit_to_letters(7).size() == 5);
+  CHECK(single_digit_to_letters(8).size() == 5);
+  CHECK(single_digit_to_letters(9).size() == 4);  
 }
 
 
