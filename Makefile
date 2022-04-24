d839593a11992323f01c832e7d2e073e1dfe4008
user <local@minipc>
2022-04-24 14:45:18 +0800

    euler problem 17: pass test_single_digit_to_letters test

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ e17:
 	g++ -g -o out/e17 euler_problem_17.cpp -I/home/user/github/doctest/doctest
 test:
 	./out/hello
-	./out/e17
+	./out/e17 -d
--- euler_problem_17.cpp
+++ euler_problem_17.cpp
@@ -1,12 +1,27 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 #include <iostream>
-
+#include <map>
 using namespace std;
 
 
-
+#define MAP_INT_STRING map <int, string>
 string number_to_words(int n){
+  MAP_INT_STRING digit_to_words;
+  digit_to_words.insert(make_pair(1,"one"));
+  digit_to_words.insert(make_pair(2,"two"));
+  digit_to_words.insert(make_pair(3,"three"));
+  digit_to_words.insert(make_pair(4,"four"));
+  digit_to_words.insert(make_pair(5,"five"));
+  digit_to_words.insert(make_pair(6,"six"));
+  digit_to_words.insert(make_pair(7,"seven"));
+  digit_to_words.insert(make_pair(8,"eight"));
+  digit_to_words.insert(make_pair(9,"nine"));
+  
+  MAP_INT_STRING::const_iterator found_element = digit_to_words.find(n);
+  if (found_element != digit_to_words.end()){
+    return found_element->second;
+  }
   return string("one");
 }
 
@@ -25,7 +40,7 @@ TEST_CASE("test_single_digit_to_letters"){
   CHECK(single_digit_to_letters(4) == 4);
   CHECK(single_digit_to_letters(5) == 4);
   CHECK(single_digit_to_letters(6) == 3);
-  CHECK(single_digit_to_letters(7) == 6);
+  CHECK(single_digit_to_letters(7) == 5);
   CHECK(single_digit_to_letters(8) == 5);
   CHECK(single_digit_to_letters(9) == 4);  
 }
